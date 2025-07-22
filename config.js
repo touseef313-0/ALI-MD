9a974a2083989e2bb6a6a45a957c74c712c097f5
touseef313-0 <touseefahmad786.p@gmail.com>
2025-07-22 12:09:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  
+SESSION_ID: process.env.SESSION_ID || "ALI-MD~zU9TGRLI#0JGMWXeOVCp93EES9h8xtuBgb_bJdb6yTE_uYCytPso",  
 //chatbot integration 
 CHATBOT: process.env.CHATBOT || "on",
 // add your Session Id 
@@ -41,8 +41,8 @@ BOT_NAME: process.env.BOT_NAME || "ALI-MD",
 STICKER_NAME: process.env.STICKER_NAME || "ALI-MD",    
 VPS: process.env.VPS || "",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-OWNER_REACT: process.env.OWNER_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
+OWNER_REACT: process.env.OWNER_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
